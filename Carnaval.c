1862170dac31715c36bb9833f8c963f819a70839
Nathan Nunes <37387448+nthnunes@users.noreply.github.com>
2018-11-28 03:01:44 -0200

    Update Carnaval.c

--- Carnaval.c
+++ Carnaval.c
@@ -6,34 +6,34 @@ int main(){
     
     scanf("%f %f %f %f %f",&X1, &X2, &X3, &X4, &X5);
     
-    if(X1<=X2&&X1<=X3&&X1<=X4&&X1<=X5){
+    if(X1<=X2 && X1<=X3 && X1<=X4 && X1<=X5){
       XMENOR=X1;
       }
-    else if(X1>=X2&&X1>=X3&&X1>=X4&&X1>=X5){
+    else if(X1>=X2 && X1>=X3 && X1>=X4 && X1>=X5){
       XMAIOR=X1;
       }
-    if(X5<=X2&&X5<=X3&&X5<=X4&&X5<=X1){
+    if(X5<=X2 && X5<=X3 && X5<=X4 && X5<=X1){
       XMENOR=X5;
       }
-    else if(X5>=X2&&X5>=X3&&X5>=X4&&X5>=X1){
+    else if(X5>=X2 && X5>=X3 && X5>=X4 && X5>=X1){
       XMAIOR=X5;
       }
-    if(X2<=X1&&X2<=X3&&X2<=X4&&X2<=X5){
+    if(X2<=X1 && X2<=X3 && X2<=X4 && X2<=X5){
       XMENOR=X2;
       }
-    else if(X2>=X5&&X2>=X3&&X2>=X4&&X2>=X1){
+    else if(X2>=X5 && X2>=X3 && X2>=X4 && X2>=X1){
       XMAIOR=X2;
       }
-    if(X3<=X2&&X3<=X4&&X3<=X5&&X3<=X1){
+    if(X3<=X2 && X3<=X4 && X3<=X5 && X3<=X1){
       XMENOR=X3;
       }
-    else if(X3>=X2&&X3>=X4&&X3>=X5&&X3>=X1){
+    else if(X3>=X2 && X3>=X4 && X3>=X5 && X3>=X1){
       XMAIOR=X3;
       }
-    if(X4<=X2&&X4<=X3&&X4<=X5&&X4<=X1){
+    if(X4<=X2 && X4<=X3 && X4<=X5 && X4<=X1){
       XMENOR=X4;
       }
-    else if(X4>=X2&&X4>=X3&&X4>=X5&&X4>=X1){
+    else if(X4>=X2 && X4>=X3 && X4>=X5 && X4>=X1){
       XMAIOR=X4;
       }
       
