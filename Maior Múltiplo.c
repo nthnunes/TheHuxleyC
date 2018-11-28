9203d03ba6bdd8716bac4729a7692bed78a6237f
Nathan Nunes <37387448+nthnunes@users.noreply.github.com>
2018-11-28 03:06:29 -0200

    Update Maior Múltiplo.c

--- Maior Múltiplo.c
+++ Maior Múltiplo.c
@@ -6,12 +6,14 @@ int main(){
   scanf("%i %i", &M, &N);
   
   if((M<=0) || (N<=0)){
-		printf("sem multiplos menores que %i", N);}
-	else if(M>N){
-		printf("sem multiplos menores que %i", N);}
-	else if(N>=M){
-		for(Y=1; Y*M<=N; Y++){}
-		printf("%i", Y*M-M);
+	printf("sem multiplos menores que %i", N);
+  	}
+  else if(M>N){
+	printf("sem multiplos menores que %i", N);
+  	}
+  else if(N>=M){
+	for(Y=1; Y*M<=N; Y++){}
+	printf("%i", Y*M-M);
     
   return 0;
   }
